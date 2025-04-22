076a35af5446cd14e3ac55fc5e71844301852aaf
Tanisha130603 <tanishakhandelwal130603@gmail.com>
2025-04-22 23:08:41 +0530

    Create accelerometer_landing.sql

---
+++ accelerometer_landing.sql
@@ -0,0 +1,12 @@
+CREATE EXTERNAL TABLE `accelerometer_landing`(
+)
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://tanisha13-lake-house/accelerometer/'
+TBLPROPERTIES (
+  'classification'='json')
