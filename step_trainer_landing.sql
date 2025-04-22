30ed39f864b3c7d802dd14248fe148deb2e59d1f
Tanisha130603 <tanishakhandelwal130603@gmail.com>
2025-04-22 23:11:27 +0530

    Create step_trainer_landing.sql

---
+++ step_trainer_landing.sql
@@ -0,0 +1,12 @@
+CREATE EXTERNAL TABLE `step_trainer_landing`(
+)
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://tanisha13-lake-house/step-trainer-landing/'
+TBLPROPERTIES (
+  'classification'='json')
