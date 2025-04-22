77a1f9d17b8ebb4844006cf862a7ca1d4a84d154
Tanisha130603 <tanishakhandelwal130603@gmail.com>
2025-04-22 23:07:46 +0530

    Create customer_landing.sql

---
+++ customer_landing.sql
@@ -0,0 +1,12 @@
+CREATE EXTERNAL TABLE `customer_landing`(
+)
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://tanisha13-lake-house/customer-landing/'
+TBLPROPERTIES (
+  'classification'='json')
